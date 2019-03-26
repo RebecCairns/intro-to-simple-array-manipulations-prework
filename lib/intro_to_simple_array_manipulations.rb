1acdb5d6473488084c8fdd7cc3a56920ab18e9cc
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-26 01:49:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,8 +8,11 @@ def using_pop(arr)
   arr.pop 
 end  
 def pop_with_args(arr)
-  deletedArrElements = arr.pop(2)
+  arr.pop(2)
 end  
 def using_shift(arr)
   arr.shift
-end   
+end  
+def shift_with_args(arr)
+  arr.shift(2)
+end  
