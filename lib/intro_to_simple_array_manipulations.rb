382e87340e007552d03b5fc19bcc99a9c44f2865
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-26 02:02:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,8 +29,8 @@ def using_flatten(arr)
   arr.flatten
 end 
 def using_delete(arr, str)
-  arr.delet(str)
+  arr.delete(str)
 end 
 def using_delete_at(arr, num)
-  arr.delet(num)
+  arr.delete(num)
 end   
